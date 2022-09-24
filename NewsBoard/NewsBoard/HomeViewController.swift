5c1073828de8c444aa357a13e31389d9567e4318
$achin Kadam <iosdev.sachin@gmail.com>
2022-09-24 13:58:46 +0530

    Add homeviewcontroller

---
+++ NewsBoard/NewsBoard/HomeViewController.swift
@@ -0,0 +1,30 @@
+//
+//  HomeViewController.swift
+//  NewsBoard
+//
+//  Created by Sachin on 24/09/22.
+//
+
+import UIKit
+
+class HomeViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .cyan
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
